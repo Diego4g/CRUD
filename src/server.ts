d6cfed794299afabb4ea35cc266a243475930265
Diego Ferreira <diegodejesusferreira2@gmail.com>
2021-12-14 17:13:23 -0300

    chore: setup preparation

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,3 @@
-import cors from "cors";
-import express, { Request, Response } from "express";
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (request: Request, response: Response) => {
-  return response.send("Hello world");
-});
+import { app } from "./app";
 
 app.listen(3333, () => console.log("Server is running in PORT 3333"));
